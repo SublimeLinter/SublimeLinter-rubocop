c17d5ae53c5044d0d9c952b1b560a7732997e55d
Mark Haylock <mhaylock@users.noreply.github.com>
2016-04-20 15:10:29 +1200

    Better path handling by using --stdin, and guessing unsaved file paths (#29)
    * Update required version to 0.34.0 This is the first version to support the new `--stdin` option, see
    https://github.com/bbatsov/rubocop/releases/tag/v0.34.0 for more
    details.
    * Switch to cmd method, use STDIN if filename exists This allows rubocop to use the real path of the file to find relevant `.rubocop.yml` config files, and for it to accurately match paths specified inside those config files against the file being linted.
    * For unsaved files, try to determine project path This allows unsaved files to use the projects root rubocop config, when possible.
    * Updated README to reflect new version requirement

--- linter.py
+++ linter.py
@@ -3,7 +3,7 @@
 # Linter for SublimeLinter3, a code checking framework for Sublime Text 3
 #
 # Written by Aparajita Fishman
-# Contributors: Francis Gulotta, Josh Hagins
+# Contributors: Francis Gulotta, Josh Hagins, Mark Haylock
 # Copyright (c) 2013 Aparajita Fishman
 #
 # License: MIT
@@ -11,6 +11,7 @@
 
 """This module exports the Rubocop plugin class."""
 
+import os
 from SublimeLinter.lint import RubyLinter
 
 
@@ -26,14 +27,47 @@ class Rubocop(RubyLinter):
         'ruby on rails',
         'ruby'
     )
-    cmd = 'ruby -S rubocop --format emacs'
+    cmd = None
+    executable = 'ruby'
     version_args = '-S rubocop --version'
     version_re = r'(?P<version>\d+\.\d+\.\d+)'
-    version_requirement = '>= 0.15.0'
+    version_requirement = '>= 0.34.0'
     regex = (
         r'^.+?:(?P<line>\d+):(?P<col>\d+): '
         r'(:?(?P<warning>[RCW])|(?P<error>[EF])): '
         r'(?P<message>.+)'
     )
-    tempfile_suffix = 'rb'
-    config_file = ('--config', '.rubocop.yml')
+
+    def cmd(self):
+        """Build command, using STDIN if a file path can be determined."""
+        command = ['ruby', '-S', 'rubocop', '--format', 'emacs']
+
+        # Set tempfile_suffix so by default a tempfile is passed onto rubocop:
+        self.tempfile_suffix = 'rb'
+
+        path = self.filename
+        if not path:
+            # File is unsaved, and by default unsaved files use the default
+            # rubocop config because they do not technically belong to a folder
+            # that might contain a custom .rubocop.yml. This means the lint
+            # results may not match the rules for the currently open project.
+            #
+            # If the current window has open folders then we can use the
+            # first open folder as a best-guess for the current projects
+            # root folder - we can then pretend that this unsaved file is
+            # inside this root folder, and rubocop will pick up on any
+            # config file if it does exist:
+            folders = self.view.window().folders()
+            if folders:
+                path = os.path.join(folders[0], 'untitled.rb')
+
+        if path:
+            # With this path we can instead pass the file contents in via STDIN
+            # and then tell rubocop to use this path (to search for config
+            # files and to use for matching against configured paths - i.e. for
+            # inheritance, inclusions and exclusions):
+            command += ['--stdin', path]
+            # Ensure the files contents are passed in via STDIN:
+            self.tempfile_suffix = None
+
+        return command
